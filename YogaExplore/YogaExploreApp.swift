094ddba3ee7a58b2df5855ae73b731d3c195bcf0
Keyi z <zjenny212@gmail.com>
2020-09-24 11:21:16 -0400

    Initial Commit

---
+++ YogaExplore/YogaExploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  YogaExploreApp.swift
+//  YogaExplore
+//
+//  Created by Keyi Zhu on 9/24/20.
+//
+
+import SwiftUI
+
+@main
+struct YogaExploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
